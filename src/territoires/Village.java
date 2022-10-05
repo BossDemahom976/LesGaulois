9e93710c281728244f8070efae15ffe20a5daf30
mohamed said youssouf houssen <mhh3664a@ens.fsi>
2022-10-05 14:02:29 +0200

    Changement village

--- src/territoires/Village.java
+++ src/territoires/Village.java
@@ -1,15 +1,21 @@
 package territoires;
 
+import personnages.Chef;
+
 public class Village {
 	private String nom;
-
+	private Chef chef;
+	
 	public Village(String nom) {
 		this.nom = nom;
 	}
-
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
 	public String getNom() {
 		return nom;
 	}
-	
 	
 }
