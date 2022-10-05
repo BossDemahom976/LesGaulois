b1c8c2b5c7ee2b86ac8fd417c4f0aab1bd204bf1
mohamed said youssouf houssen <mhh3664a@ens.fsi>
2022-10-05 14:21:01 +0200

    Changement village 2

--- src/territoires/Village.java
+++ src/territoires/Village.java
@@ -1,22 +1,43 @@
 package territoires;
 
 import personnages.Chef;
+import personnages.Gaulois;
 
 public class Village {
 	private String nom;
 	private Chef chef;
-	private int test;
+	private Gaulois[] villageois;
+	private int nbVillageois;
 	
 	public Village(String nom) {
 		this.nom = nom;
 	}
 	
-	public void setChef(Chef chef) {
+	public void setChef(Chef chef, int nbVillageosMaximum) {
 		this.chef = chef;
+		villageois = new Gaulois[nbVillageosMaximum];
+		nbVillageois = 0;
+		
 	}
 	
 	public String getNom() {
 		return nom;
 	}
 	
+	public void ajouterHabitant (Gaulois gaulois) {
+		if (nbVillageois < villageois.length) {
+			villageois[nbVillageois] = gaulois;
+			++nbVillageois;
+		}else {
+			System.out.println("Le village est plein !");
+		}
+	}
+	
+	public Gaulois trouverHabitant (int numeroVillageois) {
+			return villageois[numeroVillageois];
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Les Irréductibles");
+	}
 }
